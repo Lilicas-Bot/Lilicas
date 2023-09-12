03ca1f1a3a5f500d0e904973f72272cd0f992652
Psykka <36780964+Psykka@users.noreply.github.com>
2023-09-12 04:09:19 -0300

    feat(api): add basic hero repository

---
+++ apps/api/src/database/heroRepository.js
@@ -0,0 +1,44 @@
+import prisma from './index.js'
+
+const create = async (id, data) => {
+  return await prisma.hero.create({
+    include: {
+      itens: true,
+      party: true
+    },
+    data: {
+      guild: {
+        connect: {
+          discordId: id
+        }
+      },
+      ...data
+    }
+  })
+}
+
+/**
+ * Get or create a hero
+ * @param {string} id Discord user id
+ * @returns {import('@prisma/client').Hero[]} Hero
+ */
+const getOrCreate = async (id) => {
+  const heros = await prisma.hero.findMany({
+    where: { guild: { discordId: id } },
+    include: {
+      itens: true,
+      party: true
+    }
+  })
+
+  // check if array is empty
+  if (heros.length > 0) {
+    return heros
+  }
+
+  return await create(id, { name: 'Pepito' })
+}
+
+export {
+  getOrCreate
+}
