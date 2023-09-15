b0f4d14f719166104577c191249f32277623cafa
Gabriel Tavares <bieltmd@outlook.com>
2023-09-14 22:47:28 -0400

    feat(bot): Embed Builder

---
+++ apps/bot/src/structures/Embed.js
@@ -0,0 +1,72 @@
+export default class Embed {
+  constructor () {
+    this.type = 'rich'
+    this.color = process.env.EMBED_COLOR
+    this.fields = []
+  }
+
+  setTitle (title) {
+    this.title = title
+    return this
+  }
+
+  setDescription (description) {
+    this.description = description
+    return this
+  }
+
+  setColor (color) {
+    this.color = parseInt(color, 16)
+    return this
+  }
+
+  setAuthor (name, iconUrl, url) {
+    this.author = {
+      name,
+      url,
+      icon_url: iconUrl
+    }
+    return this
+  }
+
+  setFooter (text, iconUrl) {
+    this.footer = {
+      text,
+      icon_url: iconUrl
+    }
+    return this
+  }
+
+  setThumbnail (url, width, height) {
+    this.thumbnail = {
+      url,
+      width,
+      height
+    }
+    return this
+  }
+
+  setUrl (url) {
+    this.url = url
+    return this
+  }
+
+  setTimestamp (timestamp) {
+    this.timestamp = new Date(timestamp).toISOString()
+    return this
+  }
+
+  setImage (url, width, height) {
+    this.image = {
+      url,
+      width,
+      height
+    }
+    return this
+  }
+
+  addFields (fields) {
+    this.fields.push(...fields)
+    return this
+  }
+}
