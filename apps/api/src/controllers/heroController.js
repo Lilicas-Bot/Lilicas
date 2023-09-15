b122a77ae76ac969073a2e041e380a1a8e588c47
Psykka <36780964+Psykka@users.noreply.github.com>
2023-09-15 04:06:38 -0300

    feat(api): finish all routes

--- apps/api/src/controllers/heroController.js
+++ apps/api/src/controllers/heroController.js
@@ -2,19 +2,19 @@ import heroRepository from '../database/heroRepository.js'
 import isDataValid from '../middlewares/isDataValid.js'
 import { Router } from 'express'
 
-// const keys = Object.freeze([
-//   'name',
-//   'xp',
-//   'level',
-//   'skillPoints',
-//   'strength',
-//   'agility',
-//   'intellect',
-//   'vitality',
-//   'luck',
-//   'available',
-//   'guildId'
-// ])
+const keys = Object.freeze([
+  'name',
+  'xp',
+  'level',
+  'skillPoints',
+  'strength',
+  'agility',
+  'intellect',
+  'vitality',
+  'luck',
+  'available',
+  'guildId'
+])
 
 const route = Router()
 
@@ -27,6 +27,28 @@ route.get('/:id', isDataValid(), async (req, res) => {
 
   const player = await heroRepository.getOrCreate(discordId)
   res.status(200).json(player)
+})
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+route.post('/:id', isDataValid(), async (req, res) => {
+  const discordId = req.params.id
+  const data = await heroRepository.create(discordId, req.body)
+
+  res.status(201).json(data)
+})
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+route.put('/:id', isDataValid(keys), async (req, res) => {
+  const heroId = req.params.id
+  const data = await heroRepository.update(heroId, req.body)
+
+  res.status(200).json(data)
 })
 
 export default route
