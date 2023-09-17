40295c97a17c224d714aa8d4d428f09402147d94
zMigueel <ehomigas@gmail.com>
2023-09-16 23:43:01 -0300

    feat: countdown routes

---
+++ apps/api/src/controllers/countdownController.js
@@ -0,0 +1,30 @@
+import { Router } from 'express'
+import countdownManager from '../util/countdownManager.js'
+
+const router = new Router()
+
+router.post('/:type/:id', async (req, res) => {
+  const { id, type } = req.params
+
+  if (!id || !type) {
+    return res.status(400).json({ error: 'Missing parameters' })
+  }
+
+  await countdownManager.ensureCountdown(id, type)
+
+  res.status(201).json({ message: 'Countdown created' })
+})
+
+router.get('/:type/:id', async (req, res) => {
+  const { id, type } = req.params
+
+  if (!id || !type) {
+    return res.status(400).json({ error: 'Missing parameters' })
+  }
+
+  const isInCountdown = await countdownManager.isInCountdown(id, type)
+
+  res.status(200).json({ isInCountdown })
+})
+
+export default router
--- apps/api/src/routes.js
+++ apps/api/src/routes.js
@@ -1,11 +1,13 @@
 import { Router, json } from 'express'
 import guildController from './controllers/guildController.js'
 import heroController from './controllers/heroController.js'
+import countdownController from './controllers/countdownController.js'
 
 const router = Router()
 
 router.use(json())
 router.use('/guilds', guildController)
 router.use('/heroes', heroController)
+router.use('/countdown', countdownController)
 
 export default router
