dcca61f5d94010e45358c04089bf3ed0aad2866b
Gabriel Tavares <bieltmd@outlook.com>
2023-09-29 01:35:26 -0400

    feat(bot): Banner Command

---
+++ apps/bot/src/commands/guild/Banner.js
@@ -0,0 +1,24 @@
+import { Command } from '../../structures/index.js'
+import { success } from '../../util/Emojis.js'
+
+export default class Banner extends Command {
+  constructor (client) {
+    super(client, 'banner', {
+      description: 'Define o banner da sua Guilda',
+      options: {
+        name: 'id',
+        type: 4,
+        description: 'ID do Banner selecionado',
+        min_value: 1,
+        max_value: 48,
+        required: true
+      }
+    })
+  }
+
+  async run (interaction) {
+    const icon = `shape-${interaction.data?.options.id}`
+    await this.client.db.guilds.update(interaction.member.id, { icon })
+    return `${success} Banner da Guilda alterado com sucesso!`
+  }
+}
--- apps/bot/src/util/Constants.js
+++ apps/bot/src/util/Constants.js
@@ -1,9 +1,11 @@
+const ASSETS_URL = 'https://raw.githubusercontent.com/Lilicas-Bot/Lilicas'
 const EMBED_COLORS = { default: 0x6FA8DC }
 const MONEY_MULTIPLIER = 5
 const HERO_PORCENTAGE = 1.2
 const XP_MULTIPLIER = 10
 
 export {
+  ASSETS_URL,
   EMBED_COLORS,
   MONEY_MULTIPLIER,
   HERO_PORCENTAGE,
